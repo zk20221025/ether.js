d684acf1c5da06d864e11fdf519bbc0f4d3e5366
zk20221025 <1036767219@qq.com>
2023-05-18 14:22:27 +0800

    上传20验证

--- 20_DecodeTx/20_DecodeTx.js
+++ 20_DecodeTx/20_DecodeTx.js
@@ -1,19 +1,19 @@
 // provider.on("pending", listener)
-import { ethers,utils } from "ethers";
+import { ethers } from "ethers";
 
 // 1. 创建provider和wallet，监听事件时候推荐用wss连接而不是http
 // 准备 alchemy API 可以参考https://github.com/AmazingAng/WTFSolidity/blob/main/Topics/Tools/TOOL04_Alchemy/readme.md 
 const ALCHEMY_MAINNET_WSSURL = 'wss://eth-mainnet.g.alchemy.com/v2/SlcbMIYMG6iLTeZ67RmWlzOUbSbT8NG7';
-const provider = new ethers.providers.WebSocketProvider(ALCHEMY_MAINNET_WSSURL);
+const provider = new ethers.WebSocketProvider(ALCHEMY_MAINNET_WSSURL);
 let network = provider.getNetwork()
 network.then(res => console.log(`[${(new Date).toLocaleTimeString()}] 连接到 chain ID ${res.chainId}`));
 
 // 2. 创建interface对象，用于解码交易详情。
-const iface = new utils.Interface([
+const iface = new ethers.Interface([
     "function transfer(address, uint) public returns (bool)",
 ])
 
-    console.log("\n3. 限制访问rpc速率，不然调用频率会超出限制，报错。")
+// 3. 限制访问rpc速率，不然调用频率会超出限制，报错。
 function throttle(fn, delay) {
     let timer;
     return function(){
@@ -26,35 +26,31 @@ function throttle(fn, delay) {
         }
     }
 }
-    console.log("\n3. 运行完成")
 
 const main = async () => {
     // 4. 监听pending的erc20 transfer交易，获取交易详情，然后解码。
     console.log("\n4. 监听pending交易，获取txHash，并输出交易详情。")
     provider.on("pending", throttle(async (txHash) => {
-    if (txHash) {
-        // 获取tx详情
-        let tx = await provider.getTransaction(txHash);
-        if (tx) {
-            // filter pendingTx.data
-            if (tx.data.indexOf(iface.getSighash("exactInputSingle")) !== -1) {
-                // 打印txHash
-                console.log(`\n[${(new Date).toLocaleTimeString()}] 监听Pending交易: ${txHash} \r`);
+        if (txHash) {
+            // 获取tx详情
+            let tx = await provider.getTransaction(txHash);
+            if (tx) {
+                // filter pendingTx.data
+                if (tx.data.indexOf(iface.getFunction("transfer").selector) !== -1) {
+                    // 打印txHash
+                    console.log(`\n[${(new Date).toLocaleTimeString()}] 监听Pending交易: ${txHash} \r`);
 
-                // 打印解码的交易详情
-                let parsedTx = iface.parseTransaction(tx)
-                console.log("pending交易详情解码：")
-                console.log(parsedTx);
-                // Input data解码
-                console.log("Input Data解码：")
-                console.log(parsedTx.args);
+                    // 打印解码的交易详情
+                    let parsedTx = iface.parseTransaction(tx)
+                    console.log("pending交易详情解码：")
+                    console.log(parsedTx);
+                    // Input data解码
+                    console.log("Input Data解码：")
+                    console.log(parsedTx.args);
+                }
             }
         }
-    }
-}, 100));
-
-console.log("exit")
-
+    }, 100));
 };
 
 main()
